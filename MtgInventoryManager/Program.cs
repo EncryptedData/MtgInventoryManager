d95f962e8a89818dbfa475d6ea54249ee0f8f64d
Russell Johnson <russell.johnson@superdark.net>
2025-08-25 22:04:46 -0700

    [Serilog] Two phase logger bootstrap

--- MtgInventoryManager/Program.cs
+++ MtgInventoryManager/Program.cs
@@ -7,12 +7,18 @@ internal static class Program
     public static async Task Main(string[] args)
     {
         Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
             .WriteTo.Console()
-            .CreateLogger();
+            .CreateBootstrapLogger();
 
         try
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSerilog((services, lc) =>
+                lc.ReadFrom.Configuration(builder.Configuration).ReadFrom.Services(services).Enrich.FromLogContext()
+                    .WriteTo.Console());
+            
             var app = builder.Build();
             app.UseSerilogRequestLogging();
 
